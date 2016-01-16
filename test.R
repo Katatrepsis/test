ee717977ccc1fd3ce90adc0d807ad7fd3c1fbf93
Katatrepsis <c.hassall@leeds.ac.uk>
2016-01-16 11:07:10 +0000

    Added x,y plot
    * Extra y variable
    * x,y plot

---
+++ test.R
@@ -0,0 +1,13 @@
+# Test script for Git - Rstudio implementation
+
+x<-rnorm(100)
+hist(x)
+
+git remote add origin https://github.com/katatrepsis/test.git
+git config remote.origin.url git@github.com:katatrepsis/test.git
+git pull -u origin master
+git push -u origin master
+
+# Not sure why this isn't pushing out
+y<-c(1:100)
+plot(x,y)
